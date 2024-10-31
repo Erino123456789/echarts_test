374514ac42189751786bfe6aced5f24470e0fb4f
Erino123456789 <126383660+Erino123456789@users.noreply.github.com>
2024-10-31 19:44:12 +0900

    Update kospi_data.js

--- treemap/kospi_data.js
+++ treemap/kospi_data.js
@@ -8,16 +8,15 @@ var app = {};
 var option;
 
 myChart.showLoading();
-const household_america_2012 = 113616229;
 $.get(
   './kospi_data.json',
-  function (obama_budget_2012) {
+  function (kospi_data) {
     myChart.hideLoading();
     const visualMin = -100;
     const visualMax = 100;
     const visualMinBound = -40;
     const visualMaxBound = 40;
-    convertData(obama_budget_2012);
+    convertData(kospi_data);
     function convertData(originList) {
       let min = Infinity;
       let max = -Infinity;
@@ -25,8 +24,8 @@ $.get(
         let node = originList[i];
         if (node) {
           let value = node.value;
-          value[4] != null && value[4] < min && (min = value[4]);
-          value[4] != null && value[4] > max && (max = value[4]);
+          value[2] != null && value[2] < min && (min = value[2]);
+          value[2] != null && value[2] > max && (max = value[2]);
         }
       }
       for (let i = 0; i < originList.length; i++) {
@@ -34,25 +33,25 @@ $.get(
         if (node) {
           let value = node.value;
           // Scale value for visual effect
-          if (value[4] != null && value[4] > 0) {
-            value[5] = echarts.number.linearMap(
-              value[4],
+          if (value[2] != null && value[2] > 0) {
+            value[3] = echarts.number.linearMap(
+              value[2],
               [0, max],
               [visualMaxBound, visualMax],
               true
             );
-          } else if (value[4] != null && value[4] < 0) {
-            value[5] = echarts.number.linearMap(
-              value[4],
+          } else if (value[2] != null && value[2] < 0) {
+            value[3] = echarts.number.linearMap(
+              value[2],
               [min, 0],
               [visualMin, visualMinBound],
               true
             );
           } else {
-            value[5] = 0;
+            value[3] = 0;
           }
-          if (!isFinite(value[5])) {
-            value[5] = 0;
+          if (!isFinite(value[3])) {
+            value[3] = 0;
           }
           if (node.children) {
             convertData(node.children);
@@ -73,23 +72,33 @@ $.get(
         tooltip: {
           formatter: function (info) {
             let value = info.value;
-            let amount = value[0];
-            amount = isValidNumber(amount)
-              ? echarts.format.addCommas(amount * 1000) + '$'
+            let now_cap = value[0];
+            now_cap = isValidNumber(now_cap)
+              ? echarts.format.addCommas(now_cap) + '원'
               : '-';
-            let amount2011 = value[1];
-            amount2011 = isValidNumber(amount2011)
-              ? echarts.format.addCommas(amount2011 * 1000) + '$'
+            let pre_cap = value[1];
+            pre_cap = isValidNumber(pre_cap)
+              ? echarts.format.addCommas(pre_cap) + '원'
               : '-';
-            let change = value[4];
+            let now_price = value[1];
+            now_price = isValidNumber(now_price)
+              ? echarts.format.addCommas(now_price) + '원'
+              : '-';
+            let pre_price = value[1];
+            pre_price = isValidNumber(pre_price)
+              ? echarts.format.addCommas(pre_price) + '원'
+              : '-';
+            let change = value[2];
             change = isValidNumber(change) ? change.toFixed(2) + '%' : '-';
             return [
               '<div class="tooltip-title">' +
                 echarts.format.encodeHTML(info.name) +
                 '</div>',
-              '2012 Amount: &nbsp;&nbsp;' + amount + '<br>',
-              '2011 Amount: &nbsp;&nbsp;' + amount2011 + '<br>',
-              'Change From 2011: &nbsp;&nbsp;' + change
+              '금일시총: &nbsp;&nbsp;' + now_cap + '<br>',
+              '전일시총: &nbsp;&nbsp;' + pre_cap + '<br>',
+              '금일종가: &nbsp;&nbsp;' + now_price + '<br>',
+              '전일종가: &nbsp;&nbsp;' + pre_price + '<br>',
+              '변동율: &nbsp;&nbsp;' + change
             ].join('');
           }
         },
@@ -117,14 +126,14 @@ $.get(
                 }
               },
               {
-                color: ['#269f3c', '#aaa', '#942e38'],
+                color: ['#942e38', '#aaa', '#269f3c'],
                 colorMappingBy: 'value',
                 itemStyle: {
                   gapWidth: 1
                 }
               }
             ],
-            data: obama_budget_2012
+            data: kospi_data
           }
         ]
       })
