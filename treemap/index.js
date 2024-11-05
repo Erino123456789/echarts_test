8e18576805974ae23d38f9967a3f8bdfa07d69df
Erino123456789 <126383660+Erino123456789@users.noreply.github.com>
2024-11-05 19:30:28 +0900

    Update index.js

--- treemap/index.js
+++ treemap/index.js
@@ -345,7 +345,12 @@ function loadData(type, filename, showLoading = true, fallbackCallback = null) {
           series: [
             {
               name: `${type.toUpperCase()}`,
-              top: 80,
+              width: '100%',
+              height: '100%',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
               type: 'treemap',
               animation: true,
               upperLabel: {
